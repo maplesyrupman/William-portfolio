9c1652996a9624ce0fef4c72271f6912dbe462e7
maplesyrupman <maplesyrupman@protonmail.com>
2020-12-15 02:09:34 -0500

    nothing to see here...

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,3 @@
-//const myHeading = document.querySelector('h1');
-//myHeading.textContent = 'Hello world!' 
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!'); 
+}
